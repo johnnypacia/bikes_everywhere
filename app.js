68b39f03d2bcf17861c8b7fbea3992917a352e2a
Johnny Pacia <johnny.pacia@gmail.com>
2016-01-12 13:35:31 -0500

    adds mongo db seed  app and landing page

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+//config
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+//Delete & Edits
+app.use(methodOverride('_method')); 
+
+//Database setup
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var mongoUrl = "mongodb://localhost:27017/myDB";
+var db;
+
+MongoClient.connect(mongoUrl, function(err, database){
+	if (err){
+		console.log(err);
+	}
+	console.log("Saul B. Goodman!");
+	db = database;
+	process.on('exit', db.close);
+})
+
+//Middleware
+
+app.use(function(req, res, next){
+	console.log("body:", req.body, "params:", req.params, "query:", req.query);
+	next();
+});
+
+//CRUD
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+
+app.listen(process.env.PORT || 3000);
+
+
